2db308f7abee917f37c1bbf5eeecb2702350eea3
mncmncmnc <39808469+mncmncmnc@users.noreply.github.com>
2026-01-30 23:11:49 -0500

    restoring Kong

--- machines/8-bit-kong-drummer/vite.config.ts
+++ machines/8-bit-kong-drummer/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/machines/8-bit-kong-drummer/',
   server: {
     port: 3000,
   },
